6b64c97012d4fe6983a846e420fbc7f63dc89713
Nelson <nelsonokiki@gmail.com>
2025-05-08 10:41:56 +0100

    updated Basic_Calculator.py and added art.py

--- Basic_Calculator.py
+++ Basic_Calculator.py
@@ -1,69 +1,54 @@
-def addition(anum1, anum2):
-    return anum1 + anum2
+
+import art
 
 
-def subtraction(bnum1, bnum2):
-    return bnum1 - bnum2
+def add(n1, n2):
+    return n1 + n2
 
 
-def multiplication(cnum1, cnum2):
-    return cnum1 * cnum2
+def subtract(n1, n2):
+    return n1 - n2
 
 
-def division(fnum1, fnum2):
-    if num2 == 0:
-        return "Error! Division by zero."
-    else:
-        return fnum1 / fnum2
+def multiply(n1, n2):
+    return n1 * n2
 
 
-def operation(ope):
-    operations = {
-        '1': 'addition',
-        '2': 'subtraction',
-        '3': 'multiplication',
-        '4': 'division'
-    }
-    return operations.get(ope, 'invalid')
-
-# Basic Calculator
-print('Welcome to this Basic Calculator')
+def divide(n1, n2):
+    return n1 / n2
 
 
-while True:
-    print('\nPlease choose an operation:')
-    print('1. Addition')
-    print('2. Subtraction')
-    print('3. Multiplication')
-    print('4. Division')
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+# print(operations["*"](4, 8))
 
 
-    op = input(
-        '\nEnter the number of the operation you want to perform: ')
+def calculator():
+    print(art.logo)
+    should_accumulate = True
+    num1 = float(input("What is the first number?: "))
 
-    selected_op = operation(op)
-    print(f'\nYou have selected an {selected_op} operation')
+    while should_accumulate:
+        for symbol in operations:
+            print(symbol)
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What is the next number?: "))
+        answer = operations[operation_symbol](num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-    if selected_op == 'invalid':
-        print('Please try again.')
-        continue
+        choice = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ")
+
+        if choice == "y":
+            num1 = answer
+        else:
+            should_accumulate = False
+            print("\n" * 20)
+            calculator()
 
 
-    num1 = float(input('\nEnter first number: '))
-    num2 = float(input('Enter second number: '))
-    if op == '1':
-        print(addition(num1, num2))
-    elif op == '2':
-        print(subtraction(num1, num2))
-    elif op == '3':
-        print(multiplication(num1, num2))
-    elif op == '4':
-        print(division(num1, num2))
-    else:
-        print('Invalid operation. Please try again.')
-
-    again = input(
-        'Do you want to perform another operation? (yes/no): ').lower().strip()
-    if again != 'yes':
-        print('\nThank you for using the calculator. Goodbye!')
-        break
+calculator()
