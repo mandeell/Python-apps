ad711cf02be1b0ba5716c5681c48e9fe2616fb2b
Nelson Okiki <nelsonokiki@gmail.com>
2025-05-28 10:59:55 +0100

    update

--- day_33_start/kanye-quotes-start/main.py
+++ day_33_start/kanye-quotes-start/main.py
@@ -4,7 +4,7 @@ import requests
 
 def get_quote():
     response = requests.get("https://api.kanye.rest")
-    response.raise_for_status()
+    print(response.status_code)
     quote = response.json()["quote"]
     canvas.itemconfig(quote_text, text=quote)
     #Write your code here.
