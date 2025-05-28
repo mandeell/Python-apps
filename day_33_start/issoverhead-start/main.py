a04858efb5d8f8531ded06598031e44b60b616f9
Nelson <nelsonokiki@gmail.com>
2025-05-28 12:30:37 +0100

    updated the solution to monitor the iss using api every 60 seconds and send an email to me using smtplib

--- day_33_start/issoverhead-start/main.py
+++ day_33_start/issoverhead-start/main.py
@@ -2,6 +2,9 @@ import requests, smtplib, schedule, time
 from datetime import datetime
 from email.mime.text import MIMEText
 
+MY_LAT = 6.482050  # Your latitude
+MY_LONG = 7.501955  # Your longitude
+
 def send_email():
     my_email = "okikiviolet1@gmail.com"
     my_password = "dautribguvdblwxt"
@@ -33,20 +36,18 @@ def send_email():
         print("Connection timed out. Check internet or firewall settings.")
     except Exception as e:
         print(f"An error occurred: {e}")
-def check_iss():
-    MY_LAT = 6.482050 # Your latitude
-    MY_LONG = 7.501955 # Your longitude
-    try:
-        response = requests.get(url="http://api.open-notify.org/iss-now.json")
-        response.raise_for_status()
-        data = response.json()
+def is_iss_close():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
 
-        iss_latitude = float(data["iss_position"]["latitude"])
-        iss_longitude = float(data["iss_position"]["longitude"])
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
 
+    return (MY_LAT - 5 <= iss_latitude <= MY_LAT + 5) and (MY_LONG - 5 <= iss_longitude <= MY_LONG + 5)
         #Your position is within +5 or -5 degrees of the ISS position.
 
-
+def is_night():
         parameters = {
             "lat": MY_LAT,
             "lng": MY_LONG,
@@ -59,21 +60,15 @@ def check_iss():
         sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
         sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
-        time_now = datetime.now()
-        current_hour = time_now.hour
-        evening_hours = range(19,24)
-        morning_hours = range(0,7)
+        time_now = datetime.now().hour
 
         #If the ISS is close to my current position,
-        if ((MY_LAT -5 <= iss_latitude <= MY_LAT + 5) and (MY_LONG -5 <= iss_longitude <= MY_LONG + 5)
-                and (current_hour in evening_hours or current_hour in morning_hours)):
-            send_email()
-    except requests.RequestException as e:
-        print(f"API request failed: {e}")
-    # and it is currently dark,
-    # Then email to tell me to look up.
-    # BONUS: run the code every 60 seconds.
-schedule.every(60).seconds.do(check_iss)
+        return time_now >= sunset or time_now <= sunrise
+def check_iss_and_night():
+    if is_iss_close() and is_night():
+        send_email()
+
+schedule.every(60).seconds.do(check_iss_and_night)
 
 while True:
     schedule.run_pending()
