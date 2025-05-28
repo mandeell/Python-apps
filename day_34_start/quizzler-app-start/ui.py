11e62cc81d914b811b9bcd20b42d620baa850e9e
Nelson Okiki <nelsonokiki@gmail.com>
2025-05-28 16:57:08 +0100

    started working on the sui implementation of the trivia game app

--- day_34_start/quizzler-app-start/ui.py
+++ day_34_start/quizzler-app-start/ui.py
@@ -7,6 +7,7 @@ class QuizInterface:
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(bg=THEME_COLOR,padx=40,pady=40)
+        self.window.minsize(400,400)
         self.font_config = ("Arial", 20, "italic")
         self.canvas = Canvas(width=300,height=250,bg="white")
         self.canvas_label = self.canvas.create_text(150,125,font=self.font_config, text="Welcome to Quizzler")
@@ -15,13 +16,16 @@ class QuizInterface:
         self.score_label = Label(text="Score: 0",font=("arial",12,"bold"), bg=THEME_COLOR, fg="white")
         self.score_label.grid(row=0,column=1, pady=10)
 
-        self.true_button = Button(text="✔", font=("arial", 30), bg="#4CAF50", fg="white", width=3, height=1,
-                                  relief="flat", bd=0)
-        self.true_button.grid(row=2,column=0)
+        self.card = Frame( width=300, height=250, bg=THEME_COLOR)
+        self.card.grid(row=2, column=0, columnspan=2, pady=10)
 
-        self.false_button = Button(text="✘", font=("arial", 30), bg="#F44336", fg="white", width=3, height=1,
+        self.true_button = Button(self.card,text="✔", font=("arial", 30), bg="#4CAF50", fg="white", width=3, height=1,
                                 bd=0)
-        self.false_button.grid(row=2,column=1)
+        self.true_button.grid(row=2,column=0, padx=20)
+
+        self.false_button = Button(self.card, text="✘", font=("arial", 30), bg="#F44336", fg="white", width=3, height=1,
+                                bd=0)
+        self.false_button.grid(row=2,column=1, padx=20)
 
 
         self.window.mainloop()
